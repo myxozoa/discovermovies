fdebf59fe2176e37e5607a238f0bcbfcc62cac72
Ronnie Miksch <32121076+myxozoa@users.noreply.github.com>
2021-07-04 01:46:56 -0700

    scrolling buttons and their animations

--- src/components/ScrollingSelector/index.js
+++ src/components/ScrollingSelector/index.js
@@ -1,9 +1,29 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 import styles from "./styles.module.css";
 
+function throttle(func, limit = 200) {
+  let waiting = false;
+  return () => {
+    if (!waiting) {
+      func();
+      waiting = true;
+      setTimeout(() => (waiting = false), limit);
+    }
+  };
+}
+
 export function ScrollingSelector({ dataUrl, placeholderData, label, width = "150px", height = "225px", imgSize = "w342" }) {
   const [data, setData] = useState(placeholderData);
+  const scrollContainer = useRef(null);
+
+  const scroll = (direction) => () => {
+    if (direction === "left") {
+      scrollContainer.current.scrollBy({ left: -300, behavior: "smooth" });
+    } else {
+      scrollContainer.current.scrollBy({ left: 300, behavior: "smooth" });
+    }
+  };
 
   useEffect(() => {
     async function getData() {
@@ -23,29 +43,45 @@ export function ScrollingSelector({ dataUrl, placeholderData, label, width = "15
   return (
     <div className={styles.container}>
       <h2 className={styles.header}>{label}</h2>
-      <ul className={styles.scroller}>
-        {data.map((movie) => {
-          return (
-            <li tabIndex="1" className={styles.item} key={movie.id}>
-              <figure>
-                <picture style={{ width, height }}>
-                  <img
-                    draggable="false"
-                    className={styles.poster}
-                    loading="lazy"
-                    width="150"
-                    height="225"
-                    src={Boolean(movie.poster_path) ? `https://image.tmdb.org/t/p/${imgSize}${movie.poster_path}` : undefined}
-                    alt="Poster"
-                    style={{ width }}
-                  />
-                </picture>
-                <figcaption className={styles.title}>{movie.title || movie.name}</figcaption>
-              </figure>
-            </li>
-          );
-        })}
-      </ul>
+      <div className={styles.row}>
+        <button className={`${styles.scrollButton} ${styles.leftScrollButton}`} onClick={throttle(scroll("left"))}>
+          <div>
+            <svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 152 287">
+              <path d="M136 274L27 144 136 13" stroke-width="40" />
+            </svg>
+          </div>
+        </button>
+        <ul ref={scrollContainer} className={styles.scroller}>
+          {data.map((movie) => {
+            return (
+              <li tabIndex="1" className={styles.item} key={movie.id}>
+                <figure>
+                  <picture style={{ width, height }}>
+                    <img
+                      draggable="false"
+                      className={styles.poster}
+                      loading="lazy"
+                      width="150"
+                      height="225"
+                      src={Boolean(movie.poster_path) ? `https://image.tmdb.org/t/p/${imgSize}${movie.poster_path}` : undefined}
+                      alt="Poster"
+                      style={{ width }}
+                    />
+                  </picture>
+                  <figcaption className={styles.title}>{movie.title || movie.name}</figcaption>
+                </figure>
+              </li>
+            );
+          })}
+        </ul>
+        <button className={`${styles.scrollButton} ${styles.rightScrollButton}`} onClick={throttle(scroll("right"))}>
+          <div>
+            <svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 152 287">
+              <path d="M16 13l109 131L16 274" stroke-width="40" />
+            </svg>
+          </div>
+        </button>
+      </div>
     </div>
   );
 }
